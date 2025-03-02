ad36ab9dab0a58aa309e33b210b848887a972e8a
jomarcardoso <antonionetocardoso@gmail.com>
2025-03-03 07:24:57 +0800

    feat(server): add handlebars

--- server.ts
+++ server.ts
@@ -1,13 +1,24 @@
-import express from 'express';
+import express from "express";
+import path from "path";
+import { engine } from "express-handlebars";
 
 export class Server {
   app = express();
 
-  constructor(port = 5000) {
+  constructor(port = 3000) {
+    this.config();
     this.listen(port);
   }
 
-  listen(port = 5000) {
+  config() {
+    const { app } = this;
+    app.use(express.static("public"));
+    app.engine("handlebars", engine());
+    app.set("view engine", "handlebars");
+    app.set("views", "./views");
+  }
+
+  listen(port = 3000) {
     this.app.listen(port);
     console.log(`listen to port ${port}`);
   }
